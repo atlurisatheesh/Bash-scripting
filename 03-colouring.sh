be44047220891eb4b3b77fbe3c0297b9d2981f85
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-14 23:43:26 +0530

    Auto push

--- 03-colouring.sh
+++ 03-colouring.sh
@@ -5,3 +5,5 @@ echo -e "\e[31m I am printing red colour \e[0m"
 echo -e "\e[33m I am printing yellow colour \e[0m"
 echo -e "\e[34m I am printing blue colour \e[0m"
 echo -e "\e[35m I am printing mangenta colour \e[0m"
+
+echo -e "\e[43m ; 31m i am printing both background and fore ground colour \e[0m"
