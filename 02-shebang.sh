84cf442aec7674374aefe0ac34f24dbff31d31ef
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-14 23:10:07 +0530

    Auto push

--- 02-shebang.sh
+++ 02-shebang.sh
@@ -10,4 +10,4 @@ echo -e "fourth\nfifth\nsixth"
 
 echo -e "I\tam\tpracticing\tfrom\tshebang"
 
-echo -e "This\toutput\tis\displaying\tfrom\tshebang\tNotation\n\tHai i am from shebang basic example"
+echo -e "This\toutput\tis\ndisplaying\tfrom\tshebang\tNotation\n\tHai i am from shebang basic example"
