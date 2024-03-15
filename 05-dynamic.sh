1380fedf3bee6e559461ee203c95402397b947fa
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-15 12:03:59 +0530

    Auto push

--- 05-dynamic.sh
+++ 05-dynamic.sh
@@ -2,6 +2,9 @@
 
 DATE="$(date +%F)"
 TIME="$(date +%c)"
+Active_session="$(who | -wc -l)"
+
 
 echo "Todays date is : $DATE"
 echo "Now the time is : $TIME"
+echo "Printing the Active session: $Active_session"
