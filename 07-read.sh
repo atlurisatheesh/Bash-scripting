528118ba8075e90d735f2526abf79038d6a1ffaf
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-15 12:22:39 +0530

    Auto push

--- 07-read.sh
+++ 07-read.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 read -p "Enter the name of the person:" name
-read -e "Name of the person who ran the script lastly: $name"
+read -e "Name of the person who ran the script lastly $name"
