c2efff4c7cafa605c41db53249dcad9cb065bd8a
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-15 18:38:43 +0530

    Auto push

--- Robo-shop/components/01-frontend.sh
+++ Robo-shop/components/01-frontend.sh
@@ -9,9 +9,10 @@ fi
 echo "Installing ngninx"
    yum install nginx -y 
 
-# yum install nginx -y
-# systemctl enable nginx
-# systemctl start nginx
+echo "startin nginx"
+    systemctl enable nginx
+    systemctl start nginx
+
 
 # curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
