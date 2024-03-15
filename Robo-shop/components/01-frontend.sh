c995b6aca0453219816e478c992f1629bcbc7eff
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-15 18:48:16 +0530

    Auto push

--- Robo-shop/components/01-frontend.sh
+++ Robo-shop/components/01-frontend.sh
@@ -5,16 +5,25 @@ if [ $USER_ID -ne 0 ]; then
 echo -e "\e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
        exit 1
 fi
-
+stat() {
+    if [$1 -eq 0]; then
+        echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[32 Failure \e[0m"
+    fi
+}
 echo "Installing ngninx"
    yum install nginx -y &>> /tmp/frontend.log
+   stat $?
 
 echo "startin nginx"
     systemctl enable nginx &>> /tmp/frontend.log
     systemctl start nginx  &>> /tmp/frontend.log
+    stat $?
 
 echo "downloading the schema"
  curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+ stat $?
 
 # cd /usr/share/nginx/html
 # rm -rf *
