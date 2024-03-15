9ab12579ff70d59f0065b8721202a4beb212d4aa
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-15 12:33:45 +0530

    Auto push

---
+++ 09-commands.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+# there four types of commands are there
+# 1) Binary  (/bin   /sbin)
+# 2) Alias 
+# 3) shel Built-in command 
+# 4) Funtions (set of/ group of commands that can be written in a sequence and we can call whenever we want)
+
+# How to declare a funtion?
+# f(){
+#     message
+# }
+
+# f
+
+Demo(){
+    echo "Hai I am from command script"
+    echo "I came here to say Hai"
+    echo "I am done with that, Okay Bye"
+
+}
+Demo
