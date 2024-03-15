a93fddfee8b7965dc3c76d915444a917020320a1
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-15 12:46:01 +0530

    Auto push

--- 09-commands.sh
+++ 09-commands.sh
@@ -25,7 +25,7 @@ stat(){
 }
 ses(){
     echo "$(uptime)"
-    echo "$(uptime | awk -F : '(print $NF)' | awk -F : '(print $2)')"
+    echo "$(uptime | awk -F : '{print $NF}' | awk -F , '{print $2}')"
 }
 ses
 repo
