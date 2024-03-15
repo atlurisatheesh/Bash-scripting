cc2a33e5f87946aa2c48062f986bbff2717fcc9d
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-15 17:59:51 +0530

    Auto push

--- 12-if-else.sh
+++ 12-if-else.sh
@@ -12,3 +12,23 @@ else
     echo "printing the value of d : $d"
     exit 2
 fi
+
+ACTION=$1 
+
+if [ "$ACTION" == "start" ]; then   
+    echo -e "\e[32m Starting Payment \e[0m"
+    exit  0
+
+elif [ "$ACTION" == "stop" ]; then 
+    echo -e "\e[31m Stopping Payment \e[0m"
+    exit 1
+
+elif [ "$ACTION" == "restart" ]; then 
+    echo -e "\e[35m Restarting Payment \e[0m"
+    exit 2
+
+else 
+    echo -e "\e[35m Valid Options are start or stop or restart \e[0m"
+    echo -e "\e[33m Example Usage \e[0m \n \t bash scriptName stop"
+    exit 3
+fi
