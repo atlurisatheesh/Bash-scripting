84baf3ded52a2d9f9ae4e79486915dfeaf684ee1
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-26 18:36:38 +0530

    Auto push

--- Robo-shop/launch-ec2.sh
+++ Robo-shop/launch-ec2.sh
@@ -12,7 +12,7 @@ if [ -z $1 ] || [ -z $2 ]  ; then
 fi 
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
-AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=centos7-slurm-head-node2022-08-30T19-56-45.370Z"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
 SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=atluri | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
 
 create_ec2() {
