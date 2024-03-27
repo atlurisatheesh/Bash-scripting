50fd759afbf5fd6c8f3ecd1c027a0b3c5af6b3b1
atlurisatheesh <atlurisatheesh93@gmail.com>
2024-03-27 17:24:42 +0530

    Auto push

--- Robo-shop/launch-ec2.sh
+++ Robo-shop/launch-ec2.sh
@@ -1,55 +1,43 @@
-# #!/bin/bash 
+#!/bin/bash 
 
-# COMPONENT=$1 
-# ENV=$2
-# HOSTEDZONEID="Z074928892F9CHCZSJQ4"
-# INSTANCE_TYPE="t3.micro"
+COMPONENT=$1 
+ENV=$2
+HOSTEDZONEID="Z074928892F9CHCZSJQ4"
+INSTANCE_TYPE="t3.micro"
  
-# if [ -z $1 ] || [ -z $2 ]  ; then 
-#     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
-#     echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
-#     exit 1
-# fi 
-
-# # AMI_ID="ami-0c1d144c8fdd8d690"
-# AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
-# SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=atluri | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
-
-# create_ec2() {
-
-#     echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
-#     PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress'| sed -e 's/"//g') 
-
-#     echo -e "Private IP Address of the $COMPONENT-${ENV} is $PRIVATEIP \n\n"
-#     echo -e "Creating DNS Record of ${COMPONENT}: "
-
-#     sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
-
-#     aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
-#     echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
-# }
-
-# if [ "$1" == "all" ]; then 
-
-#     for component in mongodb catalogue cart user shipping frontend payment mysql redis rabbitmg; do 
-#         COMPONENT=$component 
-#         create_ec2
-#     done 
-
-# else 
-#         create_ec2 
-# fi 
-
-#!/bin/bash
-COMPONENT=$1
-if [ -z $1 ] ; then 
+if [ -z $1 ] || [ -z $2 ]  ; then 
     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
     echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
     exit 1
 fi 
-AMI_ID="ami-0f75a13ad2e340a58"
-SECURITY_ID="sg-0425ad184dd078256"
-INSTANCE_TYPES="t3.micro"
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPES} --security-group-ids ${SECURITY_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${COMPONENT}}]"
+
+# AMI_ID="ami-0c1d144c8fdd8d690"
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
+SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=atluri | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
+
+create_ec2() {
+
+    echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
+    PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress'| sed -e 's/"//g') 
+
+    echo -e "Private IP Address of the $COMPONENT-${ENV} is $PRIVATEIP \n\n"
+    echo -e "Creating DNS Record of ${COMPONENT}: "
+
+    sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
+
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
+    echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
+}
+
+if [ "$1" == "all" ]; then 
+
+    for component in mongodb catalogue cart user shipping frontend payment mysql redis rabbitmg; do 
+        COMPONENT=$component 
+        create_ec2
+    done 
+
+else 
+        create_ec2 
+fi 
 
 
